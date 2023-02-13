76835e7a7e072931b1726e1922bb4cdcbd577d8c
CarlosMauEscobar <125234486+CarlosMauEscobar@users.noreply.github.com>
2023-02-12 22:09:42 -0600

    Add files via upload

---
+++ CalculadoraEstaciones.js
@@ -0,0 +1,40 @@
+var mes = (prompt("Ingrese el numerop de un mes (del 1 al 12)"))-1;
+
+switch (mes){
+  case 0:
+    alert("Enero, Usted se encuentra en Invierno");
+    break;
+  case 1:
+    alert("Febrero, Usted se encuentra en Invierno");
+    break;
+  case 2:
+    alert("Marzo, Usted se encuentra en Primavera");
+    break;
+  case 3:
+    alert("Abril, Usted se encuentra en Primavera");
+    break;
+  case 4:
+    alert("Mayo, usted se encuentra en Primavera");
+    break;
+  case 5:
+    alert("Junio, Usted se encuentra en Verano");
+    break;
+  case 6:
+    alert("Julio, Usted se encuentra en Verano");
+    break;
+  case 7:
+    alert("Agosto, Usted se encuentra en Verano");
+    break;
+  case 8:
+    alert("Septiembre, Usted se encuentra en Otoño");
+    break; 
+  case 9:
+    alert("Octubre, Usted se encuentra en Otoño");
+    break;
+  case 10:
+    alert("Noviembre, Usted se encuentra en Otoño");
+    break;
+  case 11:
+    alert("Diciembre, Usted se encuentra en Invierno");
+}
+alert("Carlos Mauricio Escobar Aguilar, Carné: 22005409");
